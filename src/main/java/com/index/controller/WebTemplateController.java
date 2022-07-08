573c4c7ea5b12e46db99e98e27199d81262c7bc8
luokai <464132475@qq.com>
2022-07-08 20:30:39 +0800

    魔塔V1.0

---
+++ src/main/java/com/index/controller/WebTemplateController.java
@@ -0,0 +1,15 @@
+package com.index.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class WebTemplateController {
+
+    @GetMapping("")
+    public String index() {
+        return "redirect:/mota_nokey/index.html";
+    }
+}
---
+++ src/main/java/com/index/webTemplateApplication.java
@@ -0,0 +1,16 @@
+package com.index;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author luokai
+ */
+@SpringBootApplication
+public class webTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(webTemplateApplication.class, args);
+    }
+
+}
